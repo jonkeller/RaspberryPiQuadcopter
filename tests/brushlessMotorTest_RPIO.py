eb19af15d0f68daa32aba5eacd95ceaad04268c4
Jon Keller <kellejm2@gmail.com>
2016-02-19 10:42:39 -0500

    Refactor config, input handling

--- tests/brushlessMotorTest_RPIO.py
+++ tests/brushlessMotorTest_RPIO.py
@@ -8,21 +8,23 @@ FULL_THROTTLE = 1860
 ZERO_THROTTLE = 1060
 
 servo = PWM.Servo()
-servo.set_servo(SERVO_PIN, FULL_THROTTLE)
 
-raw_input('Now connect the power to the ESC, and press Enter...')
+# TODO: Is this even necessary?
+def config():
+    servo.set_servo(SERVO_PIN, FULL_THROTTLE)
+    raw_input('Now connect the power to the ESC, and press Enter...')
+    servo.set_servo(SERVO_PIN, ZERO_THROTTLE)
+    
 
-servo.set_servo(SERVO_PIN, ZERO_THROTTLE)
 while True:
     try:
-        us = int(raw_input('Enter num microseconds (' + str(ZERO_THROTTLE) + ' - ' + str(FULL_THROTTLE) + ') or a non-integer to quit: '))
+        throttle_value_us = int(raw_input('Enter pulse length in microseconds (' + str(ZERO_THROTTLE) + ' - ' + str(FULL_THROTTLE) + ') or a non-integer to quit: '))
+        if throttle_value_us < ZERO_THROTTLE or throttle_value_us > FULL_THROTTLE:
+            raise ValueError('Outside throttle range')
     except ValueError:
         print "Okay, stopping."
         break
-    if us < ZERO_THROTTLE or us > FULL_THROTTLE:
-        print "Okay, stopping."
-        break
 
-    servo.set_servo(SERVO_PIN, ZERO_THROTTLE)
+    servo.set_servo(SERVO_PIN, throttle_value_us)
 
 servo.stop_servo(SERVO_PIN)
