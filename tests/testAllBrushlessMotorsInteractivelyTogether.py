3d91bffa9f961d4fde8b97a8d4fb671342242051
jonkeller <kellejm2+git@gmail.com>
2016-03-16 22:26:23 -0400

    Switched to Adafruit 16-channel Servo Driver

--- tests/testAllBrushlessMotorsInteractivelyTogether.py
+++ tests/testAllBrushlessMotorsInteractivelyTogether.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python
-# Adapted from: http://withr.me/let-rpi-drive-brushless-motor/
 
+import sys
+sys.path.append('../lib/Adafruit')
+from Adafruit_PWM_Servo_Driver import PWM
 import time
-from RPIO import PWM
 
 # The following quantities are in microseconds (us):
 ZERO_THROTTLE = 530
 FULL_THROTTLE = 1860
-GRANULARITY = 10
 
-REAR_LEFT_MOTOR_PIN = 24 
-FRONT_LEFT_MOTOR_PIN = 25
-REAR_RIGHT_MOTOR_PIN = 18
-FRONT_RIGHT_MOTOR_PIN = 23
+# Set channels
+REAR_LEFT_MOTOR_CHANNEL = 11
+FRONT_LEFT_MOTOR_CHANNEL = 15
+REAR_RIGHT_MOTOR_CHANNEL = 7
+FRONT_RIGHT_MOTOR_CHANNEL = 3
 
-motors = [ REAR_LEFT_MOTOR_PIN, REAR_RIGHT_MOTOR_PIN, FRONT_LEFT_MOTOR_PIN, FRONT_RIGHT_MOTOR_PIN ]
+motors = [ REAR_LEFT_MOTOR_CHANNEL, REAR_RIGHT_MOTOR_CHANNEL, FRONT_LEFT_MOTOR_CHANNEL, FRONT_RIGHT_MOTOR_CHANNEL ]
 
-servo = PWM.Servo()
+# Adapted from https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code/tree/master/Adafruit_PWM_Servo_Driver
+
+# Initialise the PWM device using the default address
+pwm = PWM(0x40)
+pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
+
+def set_motor(channel, pulse):
+  pulseLength = 20000 
+  pulseLength /= 4096                     # 12 bits of resolution
+  pulse /= pulseLength
+  pwm.setPWM(channel, 0, pulse)
 
 def config_motors():
     for m in motors:
@@ -25,17 +36,10 @@ def config_motors():
     for m in motors:
         set_motor(m, ZERO_THROTTLE)
         time.sleep(0.5)
-#    for m in motors:
-#        set_motor(m, FULL_THROTTLE)
-#        time.sleep(0.5)
 
 def cleanup():
     for m in motors:
-        servo.stop_servo(m)
-
-def set_motor(m, us):
-    us -= us % GRANULARITY
-    servo.set_servo(m, us)
+        set_motor(m, 0)
 
 config_motors()
 
@@ -57,12 +61,12 @@ while True:
 
     #print m, throttle_value_us
     #if m & 1:
-    set_motor(REAR_LEFT_MOTOR_PIN, throttle_value_us)
+    set_motor(REAR_LEFT_MOTOR_CHANNEL, throttle_value_us)
     #if m & 2:
-    set_motor(REAR_RIGHT_MOTOR_PIN, throttle_value_us)
+    set_motor(REAR_RIGHT_MOTOR_CHANNEL, throttle_value_us)
     #if m & 4:
-    set_motor(FRONT_LEFT_MOTOR_PIN, throttle_value_us)
+    set_motor(FRONT_LEFT_MOTOR_CHANNEL, throttle_value_us)
     #if m & 8:
-    set_motor(FRONT_RIGHT_MOTOR_PIN, throttle_value_us)
+    set_motor(FRONT_RIGHT_MOTOR_CHANNEL, throttle_value_us)
 
 cleanup()
